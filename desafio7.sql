dc22da0790a1de68e52d4620898d1d0082eb4703
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-12-06 11:56:41 -0300

    requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+SELECT a.artista AS artista,
+b.album AS album,
+COUNT(c.usuario_id) AS seguidores
+FROM
+  SpotifyClone.artistas AS a
+  
+INNER JOIN
+  SpotifyClone.albuns AS b 
+  ON b.artista_id = a.artista_id
+  
+INNER JOIN
+  SpotifyClone.seguidores AS c
+ON c.artista_id = b.artista_id
+
+GROUP BY album_id	
+ORDER BY seguidores DESC, artista ASC;
