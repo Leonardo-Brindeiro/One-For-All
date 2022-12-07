6bc49fe3a5352fd88b62b5f2b7347acf708e81e7
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-12-06 22:44:48 -0300

    requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,2 +1,3 @@
-SELECT COUNT(DISTINCT usuario_id) AS quantidade_musicas_no_historico
-FROM SpotifyClone.reproducao;  
+SELECT COUNT(usuario_id) AS quantidade_musicas_no_historico FROM SpotifyClone.reproducao 
+GROUP BY usuario_id
+HAVING usuario_id= 1
